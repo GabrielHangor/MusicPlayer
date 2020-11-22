1e7f4cccd6e318d0a389d1805f40389fab0a1b3e
HangorG <gaba93@yandex.ru>
2020-11-22 21:19:38 +0300

    progress bar is changeable now

--- script.js
+++ script.js
@@ -119,6 +119,16 @@ function updateProgressBar(e) {
   }
 }
 
+// Set the timestamp on click
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("ended", nextSong);
 music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
